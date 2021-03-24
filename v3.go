28098ee808174ad916631e85a00227a84fd36c44
Balasankar Rajaguru <balasank@juniper.net>
2021-03-24 15:13:13 +0530

    remove print statement

--- v3.go
+++ v3.go
@@ -430,7 +430,6 @@ func (x *GoSNMP) unmarshalV3Header(packet []byte,
 	}
 	x.logPrintf("Parsed Security Parameters. now offset=%v,", cursor)
 
-	fmt.Println("Checking for Security Table")
 	if x.SecurityTable != nil {
 		secIdentifer := response.SecurityParameters.GetSecurityIdentifier()
 		secParamEntry, err := x.SecurityTable.LookUp(secIdentifer)
